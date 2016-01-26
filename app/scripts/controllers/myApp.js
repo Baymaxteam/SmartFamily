7921616e511fec99a2779fa0469eb884b147f5fb
Minghung.Hsieh <qwedsazxc78@gmail.com>
2016-01-26 22:36:00 +0800

    0126 schedule
    智慧控制得版型

---
+++ app/scripts/controllers/myApp.js
@@ -0,0 +1,17 @@
+var simpleList = [{name: "Moroni", age: 50, money: 50} /*,*/];
+(function() {
+  "use strict";
+
+  var app = angular.module("myApp", ["ngTable", "ngTableDemos"]);
+
+  app.controller("demoController", demoController);
+  demoController.$inject = ["NgTableParams", "ngTableSimpleList"];
+
+  function demoController(NgTableParams, simpleList) {
+    var self = this;
+    self.tableParams = new NgTableParams({}, {
+      dataset: simpleList
+    });
+  }
+})();
+
